aae26a318c6cd628cb13bd48a16a828e4fb2f67e
Arrrrny <arny@paratoner.io>
2020-05-10 16:13:25 +0300

    Acenda api nodejs client

---
+++ src/index.ts
@@ -0,0 +1,60 @@
+import axios from 'axios';
+import md5 from 'md5';
+export class Acenda {
+  constructor(public store: string, public accessToken: string) {
+  }
+
+  public async create(endPoint: string, data: any) {
+    try {
+      const url = this.urlBuilder(endPoint)
+      return await axios.post(url, data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  public async update(endPoint: string, id: number, data: any) {
+    try {
+      const url = this.urlBuilder(`${endPoint}/${id}`)
+      return await axios.put(url, data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  public async delete(endPoint: string, id: number) {
+    try {
+      const url = this.urlBuilder(`${endPoint}/${id}`)
+      return await axios.delete(url)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  public async list(endPoint: string) {
+    try {
+      const url = this.urlBuilder(endPoint)
+      return await axios.get(url)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  public async get(endPoint: string, id: number) {
+    try {
+      const url = this.urlBuilder(`${endPoint}/${id}`)
+      return await axios.get(url)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  private urlBuilder(endPoint: string): string {
+    try {
+      const hashedSlug = md5(this.store)
+      return `https://admin.acenda.com/preview/${hashedSlug}/api/${endPoint}?access_token=${this.accessToken}`
+    } catch (error) {
+      throw new Error(error)
+    }
+  }
+}
